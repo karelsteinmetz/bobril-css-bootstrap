54e7a4f02db3ba01e8721b0c5da057e5c17baba0
k.steinmetz <karel.steinmetz@gmail.com>
2016-01-12 23:52:12 +0100

    Created form inline as optional parameter.

--- src/inlineForm.ts
+++ src/inlineForm.ts
@@ -1,17 +1,19 @@
 import * as b from 'bobril';
 
 export interface IData {
-    content: b.IBobrilNode;
+    content: b.IBobrilChildren;
+    isInlined?: boolean
 }
 
 interface ICtx extends b.IBobrilCtx {
     data: IData;
 }
 
-export default b.createComponent({
+export default b.createComponent<IData>({
     render(ctx: ICtx, me: b.IBobrilNode) {
         me.tag = 'form';
-        me.className = 'form-inline';
+        if (ctx.data.isInlined)
+            me.className = 'form-inline';
         me.children = ctx.data.content;
     }
 });
